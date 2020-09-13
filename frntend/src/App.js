baba7b83e3e7b655eb72e4e03633c3420e2806d2
unknown <manujanethwidu@gmail.com>
2020-09-13 09:37:21 +0530

    1

--- frntend/src/App.js
+++ frntend/src/App.js
@@ -39,7 +39,8 @@ const App = () => {
         </aside>
         <main className="main">
           <div className="content">
-           <Route path="/" exact={tru} component={HomeScreen}/>
+            <Route path="/products/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
             <ul className="products">
               {
                 data.products.map(product =>
