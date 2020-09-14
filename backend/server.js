4f901a0177156d48072226a87742f0b52710951a
unknown <manujanethwidu@gmail.com>
2020-09-14 11:44:03 +0530

    000000

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const data =require ('./data')
+
+const app = express();
+
+app.get("/api/products", (req, res) => {
+     res.send(data.products);
+})
+
+app.listen(5000, () => {
+  console.log('Server started at http://localhost:5000');
+});
